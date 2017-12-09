fixed some bugs in the test dealing with the backprop, there is still a bug with the input and target tensors not being the right size

models/model.py
@@ -105,7 +105,7 @@ class Model(object):
             classWeights = torch.pow(torch.log(1.02 + self.histClasses / self.histClasses.max()), -1)
             #classWeights[0] = 0
 
-            self.loss = torch.nn.CrossEntropyLoss(weight=classWeights)
+            self.loss = torch.nn.CrossEntropyLoss("""weight=classWeights""")
 
             #model.cuda()
             #self.loss.cuda()