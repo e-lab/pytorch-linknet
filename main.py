c7b0672a5fc64fd51964482c860f4e1c4f81d7bf
Abhishek Chaurasia <abhishek.chaurasia29@hotmail.com>
2018-01-10 23:47:41 -0500

    revamped main function

--- main.py
+++ main.py
@@ -1,89 +1,165 @@
-import opts
-import torch
-import train
-import test
 import os
-from torch import  cuda
+import torch
+import torch.nn as nn
+from subprocess import call
+from torch.utils.data import DataLoader
+from torchvision import transforms
+import torch.nn.functional as F
+
+from train import Train
+from test import Test
+import data.segmentedData as segmentedData
+from opts import get_args # Get all the input arguments
+
+print('\033[0;0f\033[0J')
+# Color Palette
+CP_R = '\033[31m'
+CP_G = '\033[32m'
+CP_B = '\033[34m'
+CP_Y = '\033[33m'
+CP_C = '\033[0m'
+
+args = get_args() # Holds all the input arguments
+
+
+def cross_entropy2d(x, target, weight=None, size_average=True):
+# Taken from https://github.com/meetshah1995/pytorch-semseg/blob/master/ptsemseg/loss.py
+    n, c, h, w = x.size()
+    log_p = F.log_softmax(x, dim=1)
+    log_p = log_p.transpose(1, 2).transpose(2, 3).contiguous().view(-1, c)
+    log_p = log_p[target.view(n * h * w, 1).repeat(1, c) >= 0]
+    log_p = log_p.view(-1, c)
+
+    mask = target >= 0
+    target = target[mask]
+    loss = F.nll_loss(log_p, target, ignore_index=250,
+                      weight=weight, size_average=False)
+    if size_average:
+        loss /= mask.data.sum()
+    return loss
+
+
+def save_model(checkpoint, test_error, prev_error, save_dir, save_all):
+
+    if test_error <= prev_error:
+        prev_error = test_error
+
+        print(CP_G + 'Saving model!!!' + CP_C)
+        print('{}{:-<50}{}\n'.format(CP_R, '', CP_C))
+        torch.save(checkpoint, save_dir + '/model_best.pth')
+
+    if save_all:
+        torch.save(checkpoint, save_dir + '/all/model_' + str(checkpoint['epoch']) + '.pth')
+
+    torch.save(checkpoint, save_dir + '/model_resume.pth')
+
+    return prev_error
+
+
+def main():
+    print(CP_R + "e-Lab Segmentation Training Script" + CP_C)
+    #################################################################
+    # Initialization step
+    torch.manual_seed(args.seed)
+    torch.set_default_tensor_type('torch.FloatTensor')
+
+    #################################################################
+    # Load model
+    print('{}{:=<80}{}'.format(CP_R, '', CP_C))
+    print('{}Models will be saved in: {}{}'.format(CP_Y, CP_C, str(args.save)))
+    if not os.path.exists(str(args.save)):
+        os.mkdir(str(args.save))
+
+    if args.saveAll:
+        if not os.path.exists(str(args.save)+'/all'):
+            os.mkdir(str(args.save)+'/all')
+
+    epoch = 0
+    if args.resume:
+        # Load previous model state
+        checkpoint = torch.load(args.save + '/model_resume.pt')
+        epoch = checkpoint['epoch']
+        model = checkpoint['model_def']
+        model.load_state_dict(checkpoint['state_dict'])
+
+        optimizer = torch.optim.SGD(model.parameters(), args.lr,
+                momentum=args.momentum, weight_decay=args.wd)
+        optimizer.load_stat_dict(checkpoint['optim_state'])
+        print('{}Loaded model from previous checkpoint epoch # {}()'.format(CP_G, CP_C, epoch))
+    else:
+        # Load fresh model definition
+        if args.model == 'linknet':
+            # Save model definiton script
+            call(["cp", "./models/linknet.py", args.save])
+
+            from models.linknet import ModelDef
+
+        model = ModelDef()
+
+        optimizer = torch.optim.SGD(model.parameters(), args.lr,
+                momentum=args.momentum, weight_decay=args.wd)
+
+    # Criterion
+    model.cuda()
+    criterion = cross_entropy2d
+
+    #################################################################
+    # Acquire dataset loader object
+    # Normalization factor based on ResNet stats
+    prep_data = transforms.Compose([
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+            ])
+    if args.dataset == 'cs':
+        import data.segmentedData as SegmentedData
+        print ("{}Cityscapes dataset in use{}!!!".format(CP_G, CP_C))
+    else:
+        print ("{}Invalid data-loader{}".format(CP_R, CP_C))
+
+    # Training data loader
+    data_obj_train = segmentedData.SegmentedData(root=args.datapath, mode='train', transform=prep_data)
+    data_loader_train = DataLoader(data_obj_train, batch_size=args.bs, shuffle=True, num_workers=args.workers)
+    data_len_train = len(data_obj_train)
+
+    # Testing data loader
+    data_obj_test = segmentedData.SegmentedData(root=args.datapath, mode='test', transform=prep_data)
+    data_loader_test = DataLoader(data_obj_test, batch_size=args.bs, shuffle=True, num_workers=args.workers)
+    data_len_test = len(data_obj_test)
+
+    # Save arguements used for training
+    args_log = open(args.save + '/args.log', 'w')
+    args_log.write(str(args))
+    args_log.close()
+
+    error_log = list()
+    prev_error = 1000
+
+    train = Train(model, data_loader_train, optimizer, criterion, args.lr, args.wd)
+    test = Test(model, data_loader_test, criterion)
+    while epoch <= args.maxepoch:
+        train_error = train.forward()
+        test_error = test.forward()
+        print('{}{:-<80}{}'.format(CP_R, '', CP_C))
+        print('{}Epoch #: {}{:03}'.format(CP_B, CP_C, epoch))
+        print('{}Training Error: {}{:.6f} | {}Testing Error: {}{:.6f}'.format(
+            CP_B, CP_C, train_error, CP_B, CP_C, test_error))
+        error_log.append((train_error, test_error))
+
+        # Save weights and model definition
+        prev_error = save_model({
+            'epoch': epoch,
+            'model_def': ModelDef,
+            'state_dict': model.state_dict(),
+            'optim_state': optimizer.state_dict(),
+            }, test_error, prev_error, args.save, args.saveAll)
+
+    logger = open(args.save + '/error.log', 'w')
+    logger.write('{:10} {:10}'.format('Train Error', 'Test Error'))
+    logger.write('\n{:-<20}'.format(''))
+    for total_error in error_log:
+        logger.write('\n{:.6f} {:.6f}'.format(total_error[0], total_error[1]))
+
+    logger.close()
 
 
 if __name__ == '__main__':
-    opt = vars(opts.parse().parse_args())
-    torch.manual_seed(12)
-    torch.set_default_tensor_type('torch.FloatTensor')
-#################################################################
-    # cuda stuff
-    cuda.set_device(opt['devid'])
-    print('\n\27[32mModels will be saved in \27[0m\27[4m' + str(opt['save']) + '\27[0m')
-    if not os.path.exists(str(opt['save'])):
-        os.mkdir(str(opt['save']))
-    if opt['saveAll']:
-        if not os.path.exists(str(opt['save'])+'/all'):
-            os.mkdir(str(opt['save'])+'/all')
-
-#################################################################
-data = None
-if opt['dataset'] == 'cv':
-    import data.loadCamVid as DataLoader
-    data = DataLoader.CamVidDataLoader
-    opt['conClasses'] = data.conClasses, opt['Classes'] = data.classes, opt['histClasses'] = data.histClasses
-    opt['trainData'] = data.trainData, opt['testData'] = data.testData
-    print ("data is loadCamVid")
-elif opt['dataset'] == 'cs':
-    import data.loadCityscapes as DataLoader
-    data = DataLoader.CityScapeDataLoader(opt)
-    data.data_loader()
-    opt['conClasses'] = data.conClasses
-    opt['Classes'] = data.classes
-    opt['histClasses'] = data.histClasses
-    print(type(data.train_data))
-    opt['trainData'] = data.train_data
-    opt['testData'] = data.val_data
-    print ("data is loadCityscapes")
-else:
-    print ("data loader could not be found")
-
-
-#################################################################
-curr_dir = os.path.dirname(os.path.realpath(__file__))
-filename = os.path.join(curr_dir, opt['save'], 'opt.txt')
-fp = open(filename, 'w')
-for arg in opt:
-    fp.write(str(arg) + ":" + str(opt[arg]) + "\n")
-fp.close()
-
-#################################################################
-epoch = 1
-
-folder, filename = opt['model'].split('/')
-print("folder and filename:", folder, filename, "\n")
-old_path = os.getcwd()
-#os.chdir(folder)
-
-#model_raw = __import__(filename+".py") 
-#model_raw = __import__('model.py'
-if filename == 'model.py':
-    import  models.model as model
-elif filename == 'nobypass.py':
-    import models.nobypass as model
-else:
-    import models.model_res_dec as model
-
-model_init = model.Model(opt)
-Train = train.Train(model_init, opt)
-Test = test.Test(opt)
-
-
-print('\27[31m\27[4m\nTraining and testing started\27[0m')
-print('[batchSize = ' + str(opt['batchSize']) + ']')
-
-while epoch < opt['maxepoch']:
-    print('\27[31m\27[4m\nEpoch # ', epoch, '\27[0m')
-    print('==> Training:')
-    trainConf, model, loss = Train.train(opt['trainData'], opt['Classes'], epoch)
-    print('==> Testing:')
-    Test.test(opt['testData'], opt['Classes'], epoch, trainConf, model, loss)
-    trainConf = None
-
-    epoch = epoch + 1
-
-#################################################################
+    main()
--- opts.py
+++ opts.py
@@ -1,47 +1,40 @@
-import argparse
+from argparse import ArgumentParser
 
 
-def parse():
+def get_args():
     # training related
-    parser = argparse.ArgumentParser()
+    parser = ArgumentParser(description='e-Lab Segmentation Script')
     arg = parser.add_argument
-    arg('-r', '--learningRate', type=float, default=5e-4, help='learning rate, default is 5e-4')
-    arg('-d', '--learningRateDecay', type=float, default=1e-7, help='learning rate decay (in # samples), '
-                                                                                    'default is 1e-7')
-    arg('-w', '--weightDecay', type=float, default=2e-4, help='L2 penalty on the weights, default is 2e-4')
+    arg('--bs', type=float, default=8, help='batch size')
+    arg('--lr',  type=float, default=5e-4, help='learning rate, default is 5e-4')
+    arg('--lrd', type=float, default=1e-7, help='learning rate decay (in # samples)')
+    arg('--wd', type=float, default=2e-4, help='L2 penalty on the weights, default is 2e-4')
     arg('-m', '--momentum', type=float, default=.9, help='momentum, default: .9')
-    arg('-b', '--batchSize', type=float, default=8, help='batch size')
 
     # device related
+    arg('--workers', type=int, default=8, help='# of cpu threads for data-loader')
     arg('--maxepoch', type=int, default=300, help='maximum number of training epochs')
+    arg('--seed', type=int, default=0, help='seed value for random number generator')
     arg('--plot', type=bool, default=False, help='plot training/testing error')
-    arg('--lrDecayEvery', type=int, default=100, help='Decay learning rate every X epoch by 1e-1')
-    arg('-t', '--threads', type=int, default=8, help='number of threads')
-    arg('-i', '--devid', type=int, default=1, help='device ID (if using CUDA)')
     arg('--nGPU', type=int, default=4, help='number of GPUs you want to train on')
     arg('--save', type=str, default='media', help='save trained model here')
 
     # data set related:
-    arg('--channels', type=int, default=3, help='image channels')
     arg('--datapath',  type=str, default='/media/HDD1/Datasets', help='dataset location')
-    arg('--cachepath', type=str, default='/media/HDD1/cachedData/', help='cache directory to save the loaded dataset')
-    arg('--dataset',  type=str, default='cs', choices=["cv", "cvs", "cs", "su", "rp"],
-        help='dataset type: cv(CamVid)/cvs(CamVidSeg)/cs(cityscapes)/su(SUN)/rp(representation)')
+    arg('--dataset',  type=str, default='cs', choices=["cs", "cv"],
+        help='dataset type: cs(cityscapes)/cv(CamVid)')
     arg('--imHeight', type=int, default=512, help='image height  (576 cv/512 cs)')
     arg('--imWidth', type=int, default=1024, help='image width  (576 cv/512 cs)')
 
     # model related
-
-    arg('--model',  type=str, default='models/model.py', help='(default models/model.py')
+    arg('--model',  type=str, default='linknet', help='linknet')
     arg('--pretrained',  type=str, default='/media/HDD1/Models/pretrained/resnet-18.t7',
         help='pretrained encoder for which you want to train your decoder')
 
     # Saving/Displaying Information
     arg('--saveTrainConf', type=bool, default=False, help='Save training confusion matrix')
     arg('--saveAll', type=bool, default=False, help='Save all models and confusion matrices')
-    arg('--printNorm', type=bool, default=False, help='For visualize norm factor while training"')
+    arg('--resume', type=bool, default=False, help='Resume from previous checkpoint')
 
-    return parser
-
-
-
+    args = parser.parse_args()
+    return args
