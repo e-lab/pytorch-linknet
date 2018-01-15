1fcbb280acb1c9a9eea7db918b9c8a8adf1993d9
Abhishek Chaurasia <abhishek.chaurasia29@hotmail.com>
2018-01-15 18:21:07 -0500

    included visdom for plotting

--- test.py
+++ test.py
@@ -1,20 +1,34 @@
 import torch
+import visdom
+from tqdm import trange
 from torch.autograd import Variable
 
 class Test(object):
-    def __init__(self, model, data_loader, criterion):
+    def __init__(self, model, data_loader, criterion, visdom):
         super(Test, self).__init__()
         self.model = model
         self.data_loader = data_loader
         self.criterion = criterion
-
+        self.visdom = visdom
 
     def forward(self):
         self.model.eval()
         # TODO adjust learning rate
 
         total_loss = 0
-        for i, (x, yt) in enumerate(self.data_loader):
+        pbar = trange(len(self.data_loader.dataset), desc='Validation ')
+
+        if self.visdom:
+            vis = visdom.Visdom()
+
+            loss_window = vis.line(X=torch.zeros((1,)).cpu(),
+                    Y=torch.zeros((1)).cpu(),
+                    opts=dict(xlabel='minibatches',
+                    ylabel='Loss',
+                    title='Validation Loss',
+                    legend=['Loss']))
+
+        for batch_idx, (x, yt) in enumerate(self.data_loader):
             x = x.cuda(async=True)
             yt = yt.cuda(async=True)
             input_var = Variable(x, volatile=True)
@@ -24,7 +38,22 @@ class Test(object):
             output = self.model(input_var)
             loss = self.criterion(output, target_var)
 
+            if self.visdom:
+                vis.line(
+                        X=torch.ones((1, 1)).cpu() * batch_idx,
+                        Y=torch.Tensor([loss.data[0]]).unsqueeze(0).cpu(),
+                        win=loss_window,
+                        update='append')
+
             # measure accuracy and record loss
             total_loss += loss.data[0]
 
-        return total_loss
+            if batch_idx % 10 == 0:
+                if (batch_idx*len(x) + 10*(len(x))) <= len(self.data_loader.dataset):
+                    pbar.update(10 * len(x))
+                else:
+                    pbar.update(len(self.data_loader.dataset) - batch_idx*len(x))
+
+        pbar.close()
+
+        return total_loss/len(self.data_loader.dataset)
--- train.py
+++ train.py
@@ -1,9 +1,10 @@
 import torch
+import visdom
 from tqdm import trange
 from torch.autograd import Variable
 
 class Train():
-    def __init__(self, model, data_loader, optimizer, criterion, lr, wd):
+    def __init__(self, model, data_loader, optimizer, criterion, lr, wd, visdom):
         super(Train, self).__init__()
         self.model = model
         self.data_loader = data_loader
@@ -11,7 +12,7 @@ class Train():
         self.criterion = criterion
         self.lr = lr
         self.wd = wd
-
+        self.visdom = visdom
 
     def forward(self):
         self.model.train()
@@ -19,6 +20,17 @@ class Train():
 
         total_loss = 0
         pbar = trange(len(self.data_loader.dataset), desc='Training ')
+
+        if self.visdom:
+            vis = visdom.Visdom()
+
+            loss_window = vis.line(X=torch.zeros((1,)).cpu(),
+                    Y=torch.zeros((1)).cpu(),
+                    opts=dict(xlabel='minibatches',
+                    ylabel='Loss',
+                    title='Training Loss',
+                    legend=['Loss']))
+
         for batch_idx, (x, yt) in enumerate(self.data_loader):
             x = x.cuda(async=True)
             yt = yt.cuda(async=True)
@@ -29,6 +41,13 @@ class Train():
             y = self.model(input_var)
             loss = self.criterion(y, target_var)
 
+            if self.visdom:
+                vis.line(
+                        X=torch.ones((1, 1)).cpu() * batch_idx,
+                        Y=torch.Tensor([loss.data[0]]).unsqueeze(0).cpu(),
+                        win=loss_window,
+                        update='append')
+
             # measure accuracy and record loss
             total_loss += loss.data[0]
 
@@ -38,10 +57,11 @@ class Train():
             self.optimizer.step()
 
             if batch_idx % 10 == 0:
-                if (batch_idx*len(x) + 10) <= len(self.data_loader.dataset):
-                    pbar.update(10)
+                if (batch_idx*len(x) + 10*len(x)) <= len(self.data_loader.dataset):
+                    pbar.update(10 * len(x))
                 else:
                     pbar.update(len(self.data_loader.dataset) - batch_idx*len(x))
 
         pbar.close()
-        return total_loss
+
+        return total_loss/len(self.data_loader.dataset)
